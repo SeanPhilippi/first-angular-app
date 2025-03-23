a27231d178f9a0deb713171200e4c1c4a26b2809
SeanPhilippi <sean.philippi@gmail.com>
2025-03-23 12:34:13 -0400

    added new reset-counter component and 'reset' route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ResetCounterComponent } from './reset-counter/reset-counter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'reset',
+    component: ResetCounterComponent,
+  },
+];
---
+++ src/app/reset-counter/reset-counter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-counter',
+  imports: [],
+  templateUrl: './reset-counter.component.html',
+  styleUrl: './reset-counter.component.scss'
+})
+export class ResetCounterComponent {
+
+}
